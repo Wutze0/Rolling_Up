4867e5748416f2b73720f9abe44e2561d15d2474
Julian <165800544+Wutze0@users.noreply.github.com>
2025-02-21 10:30:41 +0100

    Simple Movement added
    I added simple movement to the player by pressing the arrow keys

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    public Rigidbody2D _player;
+    public GameObject _stone;
+    private float stoneRotationSpeedMultiplier = 5;
+    private float timeDifference = 0;
+    private bool _buttonpressed;
+    private int direction;
+    private float acceleration = (float) 0.5;
+    private KeyCode moveLeft = KeyCode.LeftArrow;
+    private KeyCode moveRight = KeyCode.RightArrow;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(moveRight) || Input.GetKeyDown(moveLeft))
+        {
+            _buttonpressed = true;
+
+            if (Input.GetKeyDown(moveRight))
+            {
+                direction = 1;
+            }
+            else  { direction = -1; }
+
+        }
+
+        if (Input.GetKeyUp(moveRight) || Input.GetKeyUp(moveLeft))
+        {
+            _buttonpressed = false;
+        }
+
+        if (_buttonpressed)
+        {
+            timeDifference += Time.deltaTime;
+            if(timeDifference > 0.1)
+            {
+                _player.linearVelocityX += acceleration * direction;
+                
+                timeDifference = 0;
+            }
+        }
+        _stone.transform.Rotate(0, 0, _player.linearVelocityX * stoneRotationSpeedMultiplier * -1); //-1 because a positive rotation turns to the left but we want it to turn to the right
+    }
+}
