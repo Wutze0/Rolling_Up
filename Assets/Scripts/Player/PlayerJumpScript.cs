a9b56fb0d17262d37fa47f493b7ea9f76ddcadb0
Samuel <131021077+Cannonball1234@users.noreply.github.com>
2025-04-02 10:04:49 +0200

    Added Main Menu + Settings Menu + Animation for the Player + Keybind-Settings
    Added a main menu where the player can chose between the settings (middle) and the main game (left). When entering the Settings Menu, the player can enter his preferred keybinds for "moveLeft" and "moveRight".

--- Assets/Scripts/Player/PlayerJumpScript.cs
+++ Assets/Scripts/Player/PlayerJumpScript.cs
@@ -20,29 +20,11 @@ public class PlayerJumpScript : MonoBehaviour
         }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-
-    //    if (collision.gameObject.CompareTag("Ground"))
-    //    {
-    //        isGrounded = true;
-    //    }
-
-    //}
-
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Ground"))
-    //    {
-    //        isGrounded = false;
-    //    }
-    //}
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
 
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground")) //Only increment the collidingPlatformsAmount varaible if the player is touching ground
         {
             collidingPlatformsAmount++;
         }
@@ -78,7 +60,7 @@ public class PlayerJumpScript : MonoBehaviour
         }
 
 
-        if (collidingPlatformsAmount > 0)
+        if (collidingPlatformsAmount > 0) //If the player is touching at least 1 collision, he is still able to jump
         {
             isGrounded = true;
         }
