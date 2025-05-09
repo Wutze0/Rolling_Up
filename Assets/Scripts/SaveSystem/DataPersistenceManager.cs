f351b78bfb0f50f60334fb46da356e28eed849e6
Samuel <131021077+Cannonball1234@users.noreply.github.com>
2025-05-09 09:02:45 +0200

    Added Save System
    Added a Save System that saves the position and the velocity of the player.
    
    Data is saved in a JSON-file.
    Serialization and Deserialization is implemented with Newtonsoft.

---
+++ Assets/Scripts/SaveSystem/DataPersistenceManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Rendering.UI;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+
+    [Header("File Storage Configuration")]
+    [SerializeField] private string fileName;
+    public static DataPersistenceManager instance { get; set; }
+    private List<IDataPersistence> dataPersistenceObjects;
+    private FileDataHandler fileDataHandler;
+    private GameData gameData;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        fileDataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        dataPersistenceObjects = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+    public void NewGame() //TODO: player being able to create a new game in the main scene
+    {
+        gameData = new GameData();
+    }
+
+    public void SaveGame()
+    {
+        foreach (IDataPersistence dataPersistenceObject in dataPersistenceObjects)
+        {
+            dataPersistenceObject.SaveData(gameData);
+        }
+
+
+        fileDataHandler.Save(gameData);
+    }
+    public void LoadGame()
+    {
+        gameData = fileDataHandler.Load();
+        if (gameData == null)
+        {
+            NewGame();
+        }
+
+        foreach (IDataPersistence dataPersistenceObject in dataPersistenceObjects)
+        {
+            dataPersistenceObject.LoadData(gameData);
+        }
+        //Debug.Log(gameData.playerPosition);
+        //TODO
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        MonoBehaviour[] allMonoBehaviours = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None);
+        List<IDataPersistence> dataPersistenceObjects = new List<IDataPersistence>();
+
+        foreach (var monoBehaviour in allMonoBehaviours)
+        {
+            if (monoBehaviour is IDataPersistence dataPersistence)
+            {
+                dataPersistenceObjects.Add(dataPersistence);
+            }
+        }
+
+        return dataPersistenceObjects;
+    }
+
+}
